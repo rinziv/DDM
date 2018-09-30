84ec297df0e3e8842d01db9e0d92b9a4f60263b0
Salvo Rinzivillo <rinzivillo@isti.cnr.it>
2018-09-30 12:31:42 +0200

    Tree layout using element.ui widget

--- src/components/decisiontree.vue
+++ src/components/decisiontree.vue
@@ -1,51 +1,53 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="box box-info">
-        <div class="box-header with-border">
-          <h3 class="box-title">Decision Tree Parameters</h3>
+      <div class="col-md-12">
+        <div class="box box-info">
+          <div class="box-header with-border">
+            <h3 class="box-title">Decision Tree Parameters</h3>
+          </div>
+          <!-- /.box-header -->
+          <!-- form start -->
+          <div class="box-body">
+            <el-form>
+              <el-form-item label="Target" prop="target" label-position="left">
+                <el-input v-model="params.target"></el-input>
+              </el-form-item>
+              <el-form-item label="Dataset" prop="dataset">
+                <el-select v-model="params.dataset" placeholder="Dataset">
+                  <el-option
+                    v-for="item in configuration.dataset"
+                    :key="item.key"
+                    :label="item.label" :value="item.key"
+                  ></el-option>
+                </el-select>
+              </el-form-item>
+              <el-form-item label="Split function" prop="split_function">
+                <el-select v-model="params.split_function" placeholder="Split Function">
+                  <el-option
+                    v-for="item in configuration.split_function"
+                    :key="item.key"
+                    :label="item.label" :value="item.key"
+                  ></el-option>
+                </el-select>
+              </el-form-item>
+              <el-form-item label="Min samples leaf">
+                <el-input-number v-model="params.min_samples_leaf"
+                                 :min="1" size="small"></el-input-number>
+              </el-form-item>
+              <el-form-item label="Min samples split">
+                <el-input-number v-model="params.min_samples_split"
+                                 :min="1" size="small"></el-input-number>
+              </el-form-item>
+            </el-form>
+          </div>
+          <!-- /.box-body -->
+          <div class="box-footer">
+            <!--todo: include a check for parameters constraints -->
+            <el-button type="primary" v-on:click="runComputation">Run...</el-button>
+          </div>
+          <!-- /.box-footer -->
         </div>
-        <!-- /.box-header -->
-        <!-- form start -->
-        <div class="box-body">
-          <el-form>
-            <el-form-item label="Target" prop="target" label-position="left">
-              <el-input v-model="params.target"></el-input>
-            </el-form-item>
-            <el-form-item label="Dataset" prop="dataset">
-              <el-select v-model="params.dataset" placeholder="Dataset">
-                <el-option
-                  v-for="item in configuration.dataset"
-                  :key="item.key"
-                  :label="item.label" :value="item.key"
-                ></el-option>
-              </el-select>
-            </el-form-item>
-            <el-form-item label="Split function" prop="split_function">
-              <el-select v-model="params.split_function" placeholder="Split Function">
-                <el-option
-                  v-for="item in configuration.split_function"
-                  :key="item.key"
-                  :label="item.label" :value="item.key"
-                ></el-option>
-              </el-select>
-            </el-form-item>
-            <el-form-item label="Min samples leaf">
-              <el-input-number v-model="params.min_samples_leaf"
-                               :min="1" size="small"></el-input-number>
-            </el-form-item>
-            <el-form-item label="Min samples split">
-              <el-input-number v-model="params.min_samples_split"
-                               :min="1" size="small"></el-input-number>
-            </el-form-item>
-          </el-form>
-        </div>
-        <!-- /.box-body -->
-        <div class="box-footer">
-          <!--todo: include a check for parameters constraints -->
-          <el-button type="primary" v-on:click="runComputation">Run...</el-button>
-        </div>
-        <!-- /.box-footer -->
       </div>
     </div>
     <div class="row">
@@ -77,7 +79,6 @@
           <!-- /.box-body -->
 
           <div class="box-footer">
-            Small description of the content of the box
             <!--todo: add an extensive description-->
           </div>
           <!-- /.box-footer-->
@@ -115,7 +116,6 @@
           <!-- /.box-body -->
 
           <div class="box-footer">
-            Small description of the content of the box
             <!--todo: add an extensive description-->
           </div>
           <!-- /.box-footer-->
@@ -126,13 +126,21 @@
         <!-- /.box -->
       </div>
     </div>
+    <div class="row">
+      <div class="col-md-4">
+        <el-tree :data="tree"
+                 :props="defaultProps"
+                 :default-expand-all="true"
+                 @node-click="handleNodeClick"></el-tree>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'kMeans',
+  name: 'DecisionTree',
   data() {
     return {
       title: 'Decision Tree',
@@ -143,6 +151,11 @@ export default {
         split_function: 'me',
         min_samples_leaf: 1,
         min_samples_split: 2,
+      },
+      defaultProps: {
+        'node-key': 'name',
+        children: 'children',
+        label: this.treeLabel,
       },
       configuration: {
         parameters: [
@@ -201,16 +214,16 @@ export default {
       return this.$store.getters.DecisionTreeSimulation;
     },
     training() {
-      return this.simulation.train.map((r) => {
-        const res = {};
-        r.forEach((c, i) => {
-          res[`col${i + 1}`] = c;
-        });
-        return res;
-      });
+      return this.dataset('train');
     },
     test() {
-      return this.simulation.test.map((r) => {
+      return this.dataset('test');
+    },
+    tree() {
+      return [this.simulation.tree];
+    },
+    dataset() {
+      return field => this.simulation[field].map((r) => {
         const res = {};
         r.forEach((c, i) => {
           res[`col${i + 1}`] = c;
@@ -227,6 +240,12 @@ export default {
         setter: 'DecisionTreeSimulation',
       };
       this.$store.dispatch('loadAnExperiment', payload);
+    },
+    handleNodeClick(data) {
+      console.log(data);
+    },
+    treeLabel(data, node){
+      return data.name;
     },
   },
   watch: {
